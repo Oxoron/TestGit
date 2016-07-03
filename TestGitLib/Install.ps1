2b7209089eb3a10d9e6fbad0b50f645ab6f07ae0
Oxoron30189 <oxoron30189@yandex.ru>
2016-07-03 22:17:10 +0300

    Install.ps1 script added.

---
+++ TestGitLib/Install.ps1
@@ -0,0 +1,15 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+Write-Host $toolsPath
+Write-Host $project
+
+$toolsParent = Split-Path -Path $toolsPath -Parent
+$libPath = Join-Path (Join-Path $toolsParent "lib") "4.6"
+$dllPath = Join-Path $libPath "TestGitLib.dll"
+
+Write-Host $dllPath
+if($project.Object.References)
+{
+	Write-Host "FullPath: " $dllPath
+    $project.Object.References.Add($dllPath)
+}  
